bdd684014aca1bf48ee0fe919583c0584826183d
Emil Lundström <emillund2002@gmail.com>
2023-09-07 11:49:56 +0200

    Interactable superclass template and door
    Alla interactables kan nog göras som en subklass till Interactable klassen och bara överskugga InteractSuccessful() funktionen. Liten bugg med att trigger zonen lever sitt egna liv när dörren dras ut i scener så den måste flyttas manuellt till dit dörren ligger.

---
+++ Source/Grim/InteractableObject.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractableObject.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AInteractableObject::AInteractableObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false; // no need to tick as of now 
+
+	TriggerZone = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TriggerZone"));
+	TriggerZone->SetupAttachment(RootComponent);
+
+	// bind functions for when player enters/exits the trigger zone
+	TriggerZone->OnComponentBeginOverlap.AddDynamic(this, &AInteractableObject::TriggerZoneEntered);
+	TriggerZone->OnComponentEndOverlap.AddDynamic(this, &AInteractableObject::TriggerZoneExited); 
+
+}
+
+// Called when the game starts or when spawned
+void AInteractableObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Player = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	// Enable input for this actor and bind function for when input button is pressed 
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0); 
+	
+	EnableInput(PlayerController);
+
+	PlayerController->InputComponent->BindAction(TEXT("Interact"), EInputEvent::IE_Pressed, this, &AInteractableObject::PlayerInteracted); 
+}
+
+// Called every frame
+void AInteractableObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AInteractableObject::PlayerInteracted()
+{
+	if(bPlayerIsInZone)
+		InteractSuccessful(); 
+}
+
+void AInteractableObject::TriggerZoneEntered(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor != Player)
+		return;
+
+	bPlayerIsInZone = true;
+
+	if(InteractEnterSound)
+		UGameplayStatics::PlaySoundAtLocation(this, InteractEnterSound, GetActorLocation()); 
+}
+
+void AInteractableObject::TriggerZoneExited(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int OtherBodyIndex)
+{
+	if(OtherActor != Player)
+		return;
+
+	bPlayerIsInZone = false;
+
+	if(InteractExitSound)
+		UGameplayStatics::PlaySoundAtLocation(this, InteractExitSound, GetActorLocation()); 
+}
+
+void AInteractableObject::InteractSuccessful()
+{
+	OnPlayerInteract(); 
+}
+
---
+++ Source/Grim/InteractableObject.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractableObject.generated.h"
+
+/**
+ * Base class handling interactability, when player is in zone and presses particular button.
+ * A function is called and an event is fired. Note that Tick is disabled as default. 
+ */
+
+UCLASS()
+class GRIM_API AInteractableObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractableObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// event fired when player interacts inside trigger/interactable zone 
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnPlayerInteract();
+
+	/* function called when player interacts inside trigger/interactable zone, meant to be overridden by child classes
+	* for more specific behaviour. Just be sure to still call parent function so event is fired */ 
+	virtual void InteractSuccessful();
+	
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* TriggerZone;
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY()
+	APawn* Player;
+
+	void PlayerInteracted();
+
+	// called when an actor enters the trigger zone
+	UFUNCTION()
+	void TriggerZoneEntered(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+							UPrimitiveComponent* OtherComponent, int OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// called when an actor exits the trigger zone
+	UFUNCTION()
+	void TriggerZoneExited(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+							UPrimitiveComponent* OtherComponent, int OtherBodyIndex);
+
+	bool bPlayerIsInZone = false;
+
+	// Sound to play when the player enters the trigger zone and can interact 
+	UPROPERTY(EditDefaultsOnly)
+	USoundBase* InteractEnterSound = nullptr;
+
+	// Sound to play when player exits the trigger zone and can no longer interact 
+	UPROPERTY(EditDefaultsOnly)
+	USoundBase* InteractExitSound = nullptr; 
+
+};
